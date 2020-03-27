120ecdc63e490b372bfe97bc9cb1c79e1be8294f
DimejiAre <olardi2@gmail.com>
2020-03-27 11:01:02 +0100

    implement CMP, JMP, JEQ, JNE

--- cpu.py
+++ cpu.py
@@ -10,6 +10,7 @@ class CPU:
         self.ram = [0] * 256
         self.reg = [0] * 8
         self.pc = 0
+        self.FL = 0b00000000
         self.HLT = 0b00000001
         self.LDI = 0b10000010
         self.PRN = 0b01000111
@@ -19,6 +20,10 @@ class CPU:
         self.CALL = 0b01010000
         self.RET = 0b00010001
         self.ADD = 0b10100000
+        self.CMP = 0b10100111
+        self.JMP = 0b01010100
+        self.JEQ = 0b01010101
+        self.JNE = 0b01010110
         self.bt = {
             self.LDI: self.handle_ldi,
             self.PRN: self.handle_prn,
@@ -28,7 +33,11 @@ class CPU:
             self.POP: self.handle_pop,
             self.CALL: self.handle_call,
             self.RET: self.handle_ret,
-            self.ADD: self.handle_add
+            self.ADD: self.handle_add,
+            self.CMP: self.handle_cmp,
+            self.JMP: self.handle_jmp,
+            self.JEQ: self.handle_jeq,
+            self.JNE: self.handle_jne,
         }
     
     def handle_ldi(self, operand_a, operand_b):
@@ -89,6 +98,43 @@ class CPU:
         self.alu('ADD', operand_a, operand_b)
         inc_size = (self.MUL >> 6) + 1
         return inc_size
+    
+    def handle_cmp(self, operand_a, operand_b):
+        reg_a = self.reg[operand_a]
+        reg_b = self.reg[operand_b]
+
+        if reg_a == reg_b:
+            self.FL = 0b00000001
+        elif reg_a < reg_b:
+            self.FL = 0b00000100
+        else:
+            self.FL = 0b00000010
+
+        inc_size = (self.CMP >> 6) + 1
+        return inc_size
+    
+    def handle_jmp(self, operand_a, operand_b):
+        self.pc = self.reg[operand_a]
+        inc_size = 0
+        return inc_size
+
+    def handle_jeq(self, operand_a, operand_b):
+        if (self.FL & 0b00000001):
+            self.pc = self.reg[operand_a]
+            inc_size = 0
+        else:
+            inc_size = (self.JEQ >> 6) + 1
+        return inc_size
+
+    def handle_jne(self, operand_a, operand_b):
+        if not (self.FL & 0b00000001):
+            self.pc = self.reg[operand_a]
+            inc_size = 0
+        else:
+            inc_size = (self.JEQ >> 6) + 1
+        return inc_size
+
+
 
     def handle_hlt(self, operand_a, operand_b):
         sys.exit(1)
@@ -98,18 +144,6 @@ class CPU:
         """Load a program into memory."""
 
         address = 0
-
-        # For now, we've just hardcoded a program:
-
-        # program = [
-        #     # From print8.ls8
-        #     0b10000010, # LDI R0,8
-        #     0b00000000,
-        #     0b00001000,
-        #     0b01000111, # PRN R0
-        #     0b00000000,
-        #     0b00000001, # HLT
-        # ]
 
         program = []
 
@@ -187,78 +221,3 @@ class CPU:
                 self.pc += inc_size
             else:
                 raise Exception(f"Invalid instruction {hex(IR)} at address {hex(self.pc)}")
-
-
-        """Run the CPU."""
-        # running = True
-        # inc_size = 0
-        # self.pc = 0
-        # sp = 7
-
-        # while running:
-        #     IR = self.ram[self.pc]
-
-        #     if IR == self.LDI:
-        #         operand_a = self.ram_read(self.pc + 1)
-        #         operand_b = self.ram_read(self.pc + 2)
-        #         self.reg[operand_a] = operand_b
-        #         inc_size = (self.LDI >> 6) + 1
-            
-        #     elif IR == self.PRN:
-        #         operand_a = self.ram_read(self.pc + 1)
-        #         print(self.reg[operand_a])
-        #         inc_size = (self.PRN >> 6) + 1
-
-        #     elif IR == self.MUL:
-        #         operand_a = self.ram_read(self.pc + 1)
-        #         operand_b = self.ram_read(self.pc + 2)
-        #         self.alu('MUL', operand_a, operand_b)
-        #         inc_size = (self.MUL >> 6) + 1
-
-        #     elif IR == self.PUSH:
-        #         register = self.ram_read(self.pc + 1)
-        #         val = self.reg[register]
-
-        #         self.reg[sp] -= 1
-        #         self.ram_write(self.reg[sp], val)
-        #         inc_size = (self.PUSH >> 6) + 1
-
-        #     elif IR == self.POP:
-        #         register = self.ram_read(self.pc + 1)
-        #         val = self.ram_read(self.reg[sp])
-
-        #         self.reg[register] = val
-        #         self.reg[sp] += 1
-        #         inc_size = (self.POP >> 6) + 1
-
-        #     elif IR == self.CALL:
-        #         # val = self.pc + (self.CALL >> 6) + 1
-        #         val = self.pc + 2
-        #         self.reg[sp] -= 1
-        #         self.ram_write(self.reg[sp], val)
-
-        #         register = self.ram_read(self.pc + 1)
-        #         self.pc = self.reg[register]
-        #         inc_size = 0
-            
-        #     elif IR == self.RET:
-        #         val = self.ram_read(self.reg[sp])
-        #         self.pc = val
-        #         self.reg[sp] += 1
-        #         inc_size = 0
-
-        #     elif IR == self.ADD:
-        #         operand_a = self.ram_read(self.pc + 1)
-        #         operand_b = self.ram_read(self.pc + 2)
-        #         self.alu('ADD', operand_a, operand_b)
-        #         inc_size = (self.MUL >> 6) + 1
-
-            
-        #     elif IR == self.HLT:
-        #         running = False
-
-        #     else:
-        #         print("Invalid Instruction")
-        #         running = False
-
-        #     self.pc += inc_size
